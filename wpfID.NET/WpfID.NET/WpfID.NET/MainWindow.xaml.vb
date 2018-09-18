feec1e168a69436b74edc882df3b40dccc1d0c33
Ryper101 <rdulanjith@gmail.com>
2018-09-18 23:05:34 +0530

    animations and image browse fixed

--- wpfID.NET/WpfID.NET/WpfID.NET/MainWindow.xaml.vb
+++ wpfID.NET/WpfID.NET/WpfID.NET/MainWindow.xaml.vb
@@ -1,56 +1,22 @@
-﻿Class MainWindow
+﻿Imports Microsoft.Win32
+
+Class MainWindow
     Dim lblQRclk As Integer = 0
     Dim lblSDclk As Integer = 0
-    Dim lblUDclk As Integer=0
+    Dim lblUDclk As Integer = 0
     Dim vc = New BrushConverter()
 
     Private Sub Rectangle_MouseDown(sender As Object, e As MouseButtonEventArgs)
-      mnWN.DragMove()
-    End Sub
-
-    Private Sub lblQR_MouseEnter(sender As Object, e As MouseEventArgs) Handles lblQR.MouseEnter
-        lblQR.Background = vc.ConvertFrom("#FF2D2D30")
-
-    End Sub
-
-    Private Sub lblQR_MouseLeave(sender As Object, e As MouseEventArgs) Handles lblQR.MouseLeave
-        If lblQRclk = 0 Then
-            lblQR.Background = vc.ConvertFrom("#FF141414")
-        End If
-    End Sub
-
-    Private Sub lblSD_MouseEnter(sender As Object, e As MouseEventArgs) Handles lblSD.MouseEnter
-        lblSD.Background = vc.ConvertFrom("#FF2D2D30")
-    End Sub
-
-    Private Sub lblSD_MouseLeave(sender As Object, e As MouseEventArgs) Handles lblSD.MouseLeave
-        If lblSDclk = 0 Then
-            lblSD.Background = vc.ConvertFrom("#FF141414")
-        End If
-    End Sub
-
-    Private Sub lblUD_MouseEnter(sender As Object, e As MouseEventArgs) Handles lblUD.MouseEnter
-        lblUD.Background = vc.ConvertFrom("#FF2D2D30")
-    End Sub
-
-    Private Sub lblUD_MouseLeave(sender As Object, e As MouseEventArgs) Handles lblUD.MouseLeave
-        If lblUDclk = 0 Then
-            lblUD.Background = vc.ConvertFrom("#FF141414")
-        End If
+        mnWN.DragMove()
     End Sub
 
     Private Sub lblQR_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles lblQR.MouseLeftButtonDown
-        lblQR.Background = vc.ConvertFrom("#FF2D2D30")
-        lblSD.Background = vc.ConvertFrom("#FF141414")
-        lblUD.Background = vc.ConvertFrom("#FF141414")
-
-        lblQR.FontSize = 30
+        lblQR.FontSize = 35
         lblSD.FontSize = 24
         lblUD.FontSize = 24
-
-        lblQRclk = 1
-        lblSDclk = 0
-        lblUDclk = 0
+        lblQR.Foreground = vc.ConvertFrom("#FFFFFFFF")
+        lblSD.Foreground = vc.ConvertFrom("#FFCFD9FF")
+        lblUD.Foreground = vc.ConvertFrom("#FFCFD9FF")
 
         tb1.SelectedIndex = 1
         tb1.SelectedItem = ti1
@@ -58,17 +24,12 @@
     End Sub
 
     Private Sub lblSD_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles lblSD.MouseLeftButtonDown
-        lblSD.Background = vc.ConvertFrom("#FF2D2D30")
-        lblQR.Background = vc.ConvertFrom("#FF141414")
-        lblUD.Background = vc.ConvertFrom("#FF141414")
-
         lblQR.FontSize = 24
-        lblSD.FontSize = 30
+        lblSD.FontSize = 35
         lblUD.FontSize = 24
-
-        lblQRclk = 0
-        lblSDclk = 1
-        lblUDclk = 0
+        lblQR.Foreground = vc.ConvertFrom("#FFCFD9FF")
+        lblSD.Foreground = vc.ConvertFrom("#FFFFFFFF")
+        lblUD.Foreground = vc.ConvertFrom("#FFCFD9FF")
 
         tb1.SelectedIndex = 2
         tb1.SelectedItem = ti2
@@ -77,17 +38,12 @@
     End Sub
 
     Private Sub lblUD_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles lblUD.MouseLeftButtonDown
-        lblUD.Background = vc.ConvertFrom("#FF2D2D30")
-        lblSD.Background = vc.ConvertFrom("#FF141414")
-        lblQR.Background = vc.ConvertFrom("#FF141414")
-
         lblQR.FontSize = 24
         lblSD.FontSize = 24
-        lblUD.FontSize = 30
-
-        lblQRclk = 0
-        lblSDclk = 0
-        lblUDclk = 1
+        lblUD.FontSize = 35
+        lblQR.Foreground = vc.ConvertFrom("#FFCFD9FF")
+        lblSD.Foreground = vc.ConvertFrom("#FFCFD9FF")
+        lblUD.Foreground = vc.ConvertFrom("#FFFFFFFF")
 
         tb1.SelectedIndex = 3
         tb1.SelectedItem = ti3
@@ -96,5 +52,21 @@
 
     Private Sub MainWindow_Initialized(sender As Object, e As EventArgs) Handles Me.Initialized
 
+    End Sub
+
+    Private Sub brnimg_Click(sender As Object, e As RoutedEventArgs) Handles brnimg.Click
+        Dim dlg As New OpenFileDialog()
+        dlg.InitialDirectory = "c:\"
+        dlg.Filter = "Image files (*.jpg)|*.jpg|PNG files (*.png)|*.png|All Files (*.*)|*.*"
+        dlg.RestoreDirectory = True
+
+        If dlg.ShowDialog() = True Then
+            Dim selectedFileName As String = dlg.FileName
+            Dim bitmap As New BitmapImage()
+            bitmap.BeginInit()
+            bitmap.UriSource = New Uri(selectedFileName)
+            bitmap.EndInit()
+            img1.Source = bitmap
+        End If
     End Sub
 End Class
